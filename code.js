4e23f6132ba5e8f4e04f5a416a5b65e139650f6f
WebMucidi <59727557+webmucidi@users.noreply.github.com>
2024-03-04 13:54:10 +0300

    Add files via upload

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
-//Veri havuzu tanımlanır Array/JS Object/JSON/Database
+//Veri havuzu tanımlanır Array/JS Object/JSON/Databas
+/*
 let takimlar = [
     "images/besiktas.png",
     "images/fenerbahce.png",
@@ -11,6 +12,21 @@ let takimlar = [
     "images/napoli.png",
     "images/liverpool.png"
   ];
+  */
+  const takimlar = [
+    { ad: "BEŞİKTAŞ", src: "images/besiktas.png" },
+    { ad: "FENERBAHÇE", src: "images/fenerbahce.png" },
+    { ad: "GALATASARAY", src: "images/galatasaray.png"},
+    { ad: "TRABZONSPOR", src: "images/trabzonspor.png" },
+    { ad: "MİLAN", src: "images/milan.png" },
+    { ad: "İNTER", src: "images/inter.png" },
+    { ad: "REAL MADRİD", src: "images/realmadrid.png" },
+    { ad: "BARCELONA", src: "images/barcelona.png" },
+    { ad: "NAPOLİ", src: "images/napoli.png" },
+    { ad: "LİVERPOOL", src: "images/liverpool.png" }
+
+];
+let eslesmeler=[];
 
 //Programda sık kullanılacak HTML nesneleri değişkenlere aktarılır.
 const takim1=document.getElementById("takim1");
@@ -25,18 +41,31 @@ btnEslestir.addEventListener("click",eslestir);
 function eslestir(){
     if(takimlar.length<2){
         alert("Eşleşmler tamamlandı. Tüm takımlara başarılar dileriz. GS hariç!");
-        return;
+        sonucGoster();
     }
     let rastgeleTakim1 = Math.floor(Math.random() * takimlar.length);
-    takim1.src=takimlar[rastgeleTakim1];
+    takim1.src=takimlar[rastgeleTakim1].src;
+    eslesmeler.push(takimlar[rastgeleTakim1].ad);
     takimlar.splice(rastgeleTakim1,1);
+
     let rastgeleTakim2 = Math.floor(Math.random() * takimlar.length);
-    takim2.src=takimlar[rastgeleTakim2];
+    takim2.src=takimlar[rastgeleTakim2].src;
+    eslesmeler.push(takimlar[rastgeleTakim2].ad);
     takimlar.splice(rastgeleTakim2,1);
+
     if(rastgeleTakim1==rastgeleTakim2){
         alert("Aynı takımlar birbiriyle eşleşemez. Yeniden deneyin!");
         return;
     }
+
+    console.log(takimlar,eslesmeler);
 }
 
 //Sonuç göstermek için gerekli fonksiyon tanımlanır.
+function sonucGoster(){
+    for(let i=0;i<eslesmeler.length;i=i+2){
+        const eleman=document.createElement("li");
+        sonucListe.appendChild(eleman);
+        eleman.innerHTML=eslesmeler[i]+" vs. "+eslesmeler[i+1];
+    }
+}
